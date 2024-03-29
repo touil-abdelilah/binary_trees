642eb0afc6c5fcb0e370b1c8ff449686a33aa2f3
et.touil <et.touil505@gmail.com>
2024-03-29 19:20:51 +0000

    a function that deletes an entire binary tree Prototype: void binary_tree_delete(binary_tree_t *tree); Where tree is a pointer to the root node of the tree to delete If tree is NULL, do nothing

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,18 +8,21 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
+	/* Check if tree is NULL */
 	if (tree == NULL)
 	{
-		return;
+		return;/* If tree is NULL, return */
 	}
 	else
 	{
 		if (tree != NULL)
 		{
+			/* Recursively delete the left subtree */
 			binary_tree_delete(tree->left);
-
+			/* Recursively delete the right subtree */
 			binary_tree_delete(tree->right);
 		}
+		/* Free the memory allocated for the current node */
 		free(tree);
 	}
 
