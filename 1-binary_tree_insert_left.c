93609951a1042a11bdc3456294bc5c3191217455
et.touil <et.touil505@gmail.com>
2024-03-29 16:42:59 +0000

    Betty style requirement

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,39 +2,42 @@
 
 /**
  * binary_tree_insert_left - Inserts a node as the left-child of another node
- * @parent: Pointer to the node to insert the left-child
+ * @parent: Pointer to the node to insert the left-child in
  * @value: Value to store in the new node
+ *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Check if parent is NULL */
-    if (parent == NULL)
-    {
-        return (NULL); /* Return NULL if parent is NULL */
-    }
+	/* Check if parent is NULL */
+	if (parent == NULL)
+		return (NULL);
 
-    /* Create a new node with the given value */
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-    {
-        return (NULL); /* Return NULL if memory allocation fails */
-    }
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* If parent already has a left child */
-    if (parent->left != NULL)
-    {
-        /* Set the old left child as the left child of the new node */
-        new_node->left = parent->left;
-        /* Set the parent of the old left child to the new node */
-        parent->left->parent = new_node;
-    }
+	/* Initialize new node */
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    /* Set the new node as the left child of the parent */
-    parent->left = new_node;
+	/* If parent already has a left-child */
+	if (parent->left != NULL)
+	{
+		/* Set the old left-child as the left-child of the new node */
+		new_node->left = parent->left;
+		/* Set the parent of the old left-child to the new node */
+		parent->left->parent = new_node;
+	}
 
-    return (new_node); /* Return a pointer to the new node */
+	/* Set the new node as the left-child of the parent */
+	parent->left = new_node;
+
+	return (new_node);
 }
 
