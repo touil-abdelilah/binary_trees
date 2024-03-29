43692d7563d06bac2e6b8d1709bec9b2de3abe6b
et.touil <et.touil505@gmail.com>
2024-03-29 16:29:57 +0000

    betty style requirement

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_insert_left - Inserts a node as the left-child of another node
- * @parent: Pointer to the node to insert the left-child in
+ * @parent: Pointer to the node to insert the left-child
  * @value: Value to store in the new node
- *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -12,31 +12,29 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
     /* Check if parent is NULL */
     if (parent == NULL)
-        return (NULL);
+    {
+        return (NULL); /* Return NULL if parent is NULL */
+    }
 
-    /* Allocate memory for new node */
-    new_node = malloc(sizeof(binary_tree_t));
+    /* Create a new node with the given value */
+    new_node = binary_tree_node(parent, value);
     if (new_node == NULL)
-        return (NULL);
+    {
+        return (NULL); /* Return NULL if memory allocation fails */
+    }
 
-    /* Initialize new node */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    /* If parent already has a left-child */
+    /* If parent already has a left child */
     if (parent->left != NULL)
     {
-        /* Set the old left-child as the left-child of the new node */
+        /* Set the old left child as the left child of the new node */
         new_node->left = parent->left;
-        /* Set the parent of the old left-child to the new node */
+        /* Set the parent of the old left child to the new node */
         parent->left->parent = new_node;
     }
 
-    /* Set the new node as the left-child of the parent */
+    /* Set the new node as the left child of the parent */
     parent->left = new_node;
 
-    return (new_node);
+    return (new_node); /* Return a pointer to the new node */
 }
 
