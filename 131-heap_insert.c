a17ca5a4a0e733a651789559bc69d59868b85af2
et.touil <et.touil505@gmail.com>
2024-03-30 00:56:53 +0000

    a function that inserts a value in Max Binary Heap

---
+++ 131-heap_insert.c
@@ -0,0 +1,63 @@
+
+#include "binary_trees.h"
+
+heap_t *heap_insert(heap_t **root, int value);
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/**
+ *  * heap_insert- Inserts Max Binary Heap.
+ *   *
+ *    * @root: A double pointer to the root node of the Heap.
+ *     * @value: The value to store node.
+ *      *
+ *       * Return: A pointer created node.
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *tree, *new, *flip;
+	int size, leaves, sub, bit, level, tmp;
+
+	if (!root)
+		return (NULL);
+	if (!(*root))
+		return (*root = binary_tree_node(NULL, value));
+
+	tree = *root;
+	size = binary_tree_size(tree);
+	leaves = size;
+
+	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
+		leaves -= sub;
+	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
+		tree = leaves & bit ? tree->right : tree->left;
+
+	new = binary_tree_node(tree, value);
+	leaves & 1 ? (tree->right = new) : (tree->left = new);
+
+	flip = new;
+	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
+	{
+		tmp = flip->n;
+		flip->n = flip->parent->n;
+		flip->parent->n = tmp;
+		new = new->parent;
+	}
+
+	return (new);
+}
+
+/**
+ *  * binary_tree_size - Measures size of binary tree.
+ *   *
+ *    * @tree: A pointer to the tree.
+ *     *
+ *      * Return: Size of the tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
